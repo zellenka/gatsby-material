7115a7d4ccfa5b212dc318f3a69f6cfb97bf5ef6
Serhii <seleshhenko@yahoo.com>
2019-09-24 16:52:51 +0300

    wp sinc

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+// gatsby-node.js
+
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -5,3 +7,39 @@
  */
 
 // You can delete this file if you're not using it
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const BlogPostTemplate = path.resolve("./src/components/templates/BlogPost.js")
+
+  const result = await graphql(`
+    {
+      allWordpressPost {
+        edges {
+          node {
+            slug
+            wordpress_id
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  const BlogPosts = result.data.allWordpressPost.edges
+  BlogPosts.forEach(post => {
+    createPage({
+      path: `/post/${post.node.slug}`,
+      component: BlogPostTemplate,
+      context: {
+        id: post.node.wordpress_id,
+      },
+    })
+  })
+}
---
+++ src/components/templates/BlogPost.js
@@ -0,0 +1,37 @@
+
+import React from "react"
+import Layout from "../layout"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import SEO from "../seo"
+
+const BlogPostTemplate = ({ data }) => (
+  <Layout>
+    <SEO
+      title={data.wordpressPost.title}
+      description={data.wordpressPost.excerpt}
+    />
+    <h1>{data.wordpressPost.title}</h1>
+    <p>
+      Written by {data.wordpressPost.author.name} on {data.wordpressPost.date}
+    </p>
+    <div
+      style={{ marginTop: 20 }}
+      dangerouslySetInnerHTML={{ __html: data.wordpressPost.content }}
+    />
+  </Layout>
+)
+export default BlogPostTemplate
+export const query = graphql`
+  query($id: Int!) {
+    wordpressPost(wordpress_id: { eq: $id }) {
+      title
+      content
+      excerpt
+      date(formatString: "MMMM DD, YYYY")
+      author {
+        name
+      }
+    }
+  }
+`
